1c3265bab92be68249937d0f3d71c3cc0deacaa1
Rod Falanga <Rod-in-NM@users.noreply.github.com>
2022-06-08 14:40:58 -0600

    Simple project to see if .NET 6 is installed

---
+++ HelloWorldDotNet6/HelloWorldDotNet6/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine();
+Console.WriteLine("This console app was created using VS 2022 and .NET 6");
